b939cee92c679bc33130dd5db5855f4c2e6bc396
magnifiques <arpit.vaghela@outlook.com>
2025-09-27 20:57:47 -0400

    fix the output and cuda error

--- chroma_utils.py
+++ chroma_utils.py
@@ -4,7 +4,8 @@ from langchain.embeddings import HuggingFaceEmbeddings
 def get_chroma_db():
     
     embedding_function = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        device="cpu"
     )
 
     vectordb = Chroma(
